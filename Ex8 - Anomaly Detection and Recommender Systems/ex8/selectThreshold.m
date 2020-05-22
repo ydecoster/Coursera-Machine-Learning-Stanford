6f155fe2a1557a72e30d385a52894663dfc8afd0
ydecoster <65758139+ydecoster@users.noreply.github.com>
2020-05-22 11:30:49 +0200

    Upload 3
    Upload 3

---
+++ Ex8 - Anomaly Detection and Recommender Systems/ex8/selectThreshold.m
@@ -0,0 +1,46 @@
+function [bestEpsilon bestF1] = selectThreshold(yval, pval)
+%SELECTTHRESHOLD Find the best threshold (epsilon) to use for selecting
+%outliers
+%   [bestEpsilon bestF1] = SELECTTHRESHOLD(yval, pval) finds the best
+%   threshold to use for selecting outliers based on the results from a
+%   validation set (pval) and the ground truth (yval).
+%
+
+bestEpsilon = 0;
+bestF1 = 0;
+F1 = 0;
+
+stepsize = (max(pval) - min(pval)) / 1000;
+for epsilon = min(pval):stepsize:max(pval)
+    
+    % ====================== YOUR CODE HERE ======================
+    % Instructions: Compute the F1 score of choosing epsilon as the
+    %               threshold and place the value in F1. The code at the
+    %               end of the loop will compare the F1 score for this
+    %               choice of epsilon and set it to be the best epsilon if
+    %               it is better than the current choice of epsilon.
+    %               
+    % Note: You can use predictions = (pval < epsilon) to get a binary vector
+    %       of 0's and 1's of the outlier predictions
+
+    %We start by predicting the value of y with the current loop epsilon
+    predictions = (pval < epsilon);
+    %Let's calculate the number of true positives
+    tp = sum((predictions==yval)&(yval==1));
+    %Let's calculate the precision, which is tp divided by the number of positive predictions
+    prec = tp / (sum(predictions==1));
+    %Now the recall which is tp divided by the number of real positive
+    rec = tp / (sum(yval==1));
+    %Calculation of F1
+    F1 = 2*prec*rec / (prec + rec);
+
+
+    % =============================================================
+
+    if F1 > bestF1
+       bestF1 = F1;
+       bestEpsilon = epsilon;
+    end
+end
+
+end
